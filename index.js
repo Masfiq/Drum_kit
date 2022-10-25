5f9375867f27723d2326f1cbba4f7e2d37a9c602
Masfiq <rezamasfiq@gmail.com>
2022-10-25 09:44:02 +0600

    added some files

---
+++ index.js
@@ -0,0 +1,78 @@
+// document.querySelector("button").addEventListener("click",handleClick);
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons ; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+
+}
+
+//detect mouse click
+function handleClick(){
+    // this.style.color = "white";
+    // var audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
+
+    var buttoninnerHTML = this.innerHTML;
+    makeSound(buttoninnerHTML);
+    buttonAnimation(buttoninnerHTML);
+      
+}
+
+//detect keyboard press
+document.addEventListener("keydown",function(event){
+    var k =  event.key;
+    makeSound(k); 
+    buttonAnimation(k);
+
+})
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+            
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        
+         case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        
+        default:console.log(key);
+            break;
+
+    }        
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+   }, 100); //Time before execution in millisecond
+}
